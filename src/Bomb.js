155986fb2cb7d8a2b55797d3209b46e3d3c918e4
Sonia Dumitru <soni.dumitru@gmail.com>
2019-06-07 13:03:57 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,21 @@
 // your Bomb code here!
+import React from 'react'
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      secondsLeft: props.initialCount //the initial state set based on by passing props in the constructor
+    }
+  }
+  render() {
+
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return(
+      <div>
+        {message}
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ImageSlider from './ImageSlider';
 import Bomb from './Bomb';
-
 ReactDOM.render(
   <div>
     <ImageSlider />
